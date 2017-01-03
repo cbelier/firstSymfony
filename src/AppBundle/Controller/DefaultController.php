a3e6db1be4ca0483551ec235c4e966856fce6791
Your Name <you@example.com>
2017-01-03 11:00:19 +0100

    Sauvegarde j3 V0.1

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+  namespace AppBundle\Controller;
+  /* src/AppBundle/Controller/DefaultController.php  */
+  
+class DefaultController {
+  
+  public function __construct() {
+  		echo 'Instanciation de la class DefaultController</br>';
+  		echo 'On est a Paris !';
+  }
+  
+}
